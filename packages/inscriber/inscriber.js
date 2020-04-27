5bb046540b145a2c2e58b4787ec63df339f0e4bd
Sankar Ganesh <sankarg@freshdesk.com>
2020-04-27 17:10:01 +0530

    Refactored to class structure to accomodate inheritance

--- packages/inscriber/inscriber.js
+++ packages/inscriber/inscriber.js
@@ -1,31 +1,105 @@
-var Inscriber = function(compute) {
-  var value, changed = true;
+class Inscriber {
+  constructor() {
+    this._datasets = {};
+    this._bindings = {};
+  }
 
-  return {
-    compute: compute || function() {
-      return undefined;
-    },
+  _setupDatasets(name, properties, method) {
+    this._datasets[name] = {
+      properties: properties,
+      method: method,
+      changed: true
+    };
+  }
 
-    toggle: function() {
-      changed = changed || true;
-    },
-
-    reset: function() {
-      changed = changed && false;
-    },
-
-    value: function() {
-      if (changed) {
-        if (this.compute && typeof this.compute === "function") {
-          value = this.compute();
-        } else {
-          value = undefined;
-        }
-        this.reset();
+  _defineProperty(name) {
+    var that = this;
+    Object.defineProperty(this, name, {
+      get: function() {
+        return that._getValue(name);
       }
-      return value;
+    });
+  }
+
+  _setupBindings(name) {
+    let data = this._datasets[name],
+        properties = data && data.properties;
+    
+    properties.forEach((property) => {
+      this._bindProperty(name, property);
+    });
+  }
+  
+  _getValue(name) {
+    let data = this._datasets[name],
+        isChanged = data && data.changed,
+        method = data && data.method;
+  
+    if (isChanged) {
+      data.value = method.apply(this);
+      this.reset(name);
+    }
+    return data.value;
+  }
+  
+  _bindProperty(name, property) {
+    var that = this;
+  
+    let data = this._datasets[name],
+        binding = this._bindings[property];
+    
+    if (binding) {
+      if (!binding.includes(name)) {
+        binding.push(name);
+      }
+    } else {
+      let propValue = this[property];
+      this._bindings[property] = [name];
+      Object.defineProperty(this, property, {
+        get: function() {
+          return propValue;
+        },
+        set: function(value) {
+            propValue = value;
+            that.toggle(name);
+        }
+      }); 
     }
   }
-};
+
+  compute(name, properties, method) {
+    if (!name) {
+      return;
+    }
+    if (!properties || properties.length === 0) {
+      return;
+    }
+    if (!method || typeof method !== 'function') {
+      return;
+    }
+
+    let nameTaken = this._datasets[name];
+    // Call dataset always to keep properties and binding up-to-date
+    this._setupDatasets(name, properties, method);
+    
+    // Define property only when name is not taken
+    if (!nameTaken) {
+      this._defineProperty(name);
+    }
+
+    // Call bindings always to keep properties and bindings up-to-date
+    this._setupBindings(name);
+  }
+  
+  toggle(name) {
+    let data = this._datasets[name];
+    data.changed = data.changed || true;
+  }
+  
+  reset(name) {
+    let data = this._datasets[name];
+    data.changed = data.changed && false;
+  }
+}
 
 export default Inscriber;
--- packages/inscriber/test/test.js
+++ packages/inscriber/test/test.js
@@ -4,82 +4,150 @@ const sinon = require('sinon');
 import Inscriber from '../inscriber';
 
 describe('Inscriber Test', function() {
-  it('check inscribe is instantiable', function() {
-    let compute = () => {};
-    let inscriber = Inscriber(compute);
-    
+  it('check inscriber is instantiable', function() {
+    let inscriber = new Inscriber();
     assert.equal(typeof inscriber, 'object');
-    assert.equal(typeof inscriber.value, 'function');
+    assert.equal(typeof inscriber.compute, 'function');
     assert.equal(typeof inscriber.toggle, 'function');
     assert.equal(typeof inscriber.reset, 'function');
-    assert.deepEqual(inscriber.compute, compute);
   });
 
-  it('check inscribe compute exist by default', function() {
-    let inscriber = Inscriber();
+  it('check inscriber compute has default value', function() {
+    let inscriber = new Inscriber();
+    inscriber.compute('fullName', ['firstName', 'lastName'], void 0);
+    assert.equal(inscriber.fullName, void 0);
+  });
+
+  it('check inscriber compute has no default value', function() {
+    let inscriber = new Inscriber();
+    inscriber.firstName = 'Sankar';
+    inscriber.lastName = 'Ganesh';    
+
+    inscriber.compute('', [], void 0);
+    assert.equal(inscriber.fullName, void 0);
+
+    inscriber.compute('fullName', [], void 0);
+    assert.equal(inscriber.fullName, void 0);
+
+    inscriber.compute('fullName', ['firstName', 'lastName'], {});
+    assert.equal(inscriber.fullName, void 0);
+  });
+
+  it('check inscriber compute works as expected', function() {
+    let inscriber = new Inscriber();
+    inscriber.firstName = 'Sankar';
+    inscriber.lastName = 'Ganesh';
+    let compute = function() {
+      return `${this.firstName} ${this.lastName}`;
+    };
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
+    assert.equal(inscriber.fullName, 'Sankar Ganesh');
+    inscriber.firstName = 'Selva';
+    assert.equal(inscriber.fullName, 'Selva Ganesh');
+    inscriber.lastName = 'Raj';
+    assert.equal(inscriber.fullName, 'Selva Raj');
+  });
+
+  it('check inscriber compute function is cached', function() {
+    let compute = sinon.fake();
+    let inscriber = new Inscriber();
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
+
+    let fullName = inscriber.fullName;
+    assert.ok(compute.called);
+    compute.resetHistory();
+
+    fullName = inscriber.fullName;
+    assert.notOk(compute.called);
     
-    assert.equal(typeof inscriber, 'object');
-    assert.equal(typeof inscriber.value, 'function');
-    assert.equal(typeof inscriber.toggle, 'function');
-    assert.equal(typeof inscriber.reset, 'function');
-    assert.equal(inscriber.compute(), undefined);
-    assert.deepEqual(typeof inscriber.compute, 'function');
-  });
-
-  it('check inscribe did not recalculate the value', function() {
-    let compute = sinon.fake();
-    let inscriber = Inscriber(compute);
-
-    inscriber.value();
+    inscriber.firstName = 'Sankar';
+    fullName = inscriber.fullName;
     assert.ok(compute.called);
     compute.resetHistory();
 
-    assert.notOk(compute.called);
-    inscriber.value();
-    assert.notOk(compute.called);
+    inscriber.lastName = 'Ganesh';
+    fullName = inscriber.fullName;
+    assert.ok(compute.called);
   });
 
-  it('check inscribe recalculate the value on toggle', function() {
+  it('check inscriber compute function cache is destroyed on toggle', function() {
     let compute = sinon.fake();
-    let inscriber = Inscriber(compute);
+    let inscriber = new Inscriber();
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
 
-    inscriber.value();
+    let fullName = inscriber.fullName;
     assert.ok(compute.called);
     compute.resetHistory();
-
-    assert.notOk(compute.called);
-    inscriber.toggle();
-    inscriber.value();
+    
+    inscriber.toggle('fullName');
+    fullName = inscriber.fullName;
     assert.ok(compute.called);
   });
 
-  it('check inscribe did not recalculate the value on reset', function() {
+  it('check inscriber compute function cache is maintained on reset', function() {
     let compute = sinon.fake();
-    let inscriber = Inscriber(compute);
+    let inscriber = new Inscriber();
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
 
-    inscriber.value();
+    let fullName = inscriber.fullName;
     assert.ok(compute.called);
     compute.resetHistory();
-
-    assert.notOk(compute.called);
-    inscriber.reset();
-    inscriber.value();
+    
+    inscriber.reset('fullName');
+    fullName = inscriber.fullName;
     assert.notOk(compute.called);
   });
 
-  it('check compute is returnable', function() {
-    let compute = () => { return 1; };
-    let inscriber = Inscriber(compute);
+  it('check inscriber compute allows overriding', function() {
+    let inscriber = new Inscriber();
+    inscriber.firstName = 'Sankar';
+    inscriber.lastName = 'Ganesh';
+    let compute = function() {
+      return `${this.firstName} ${this.lastName}`;
+    };
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
+    assert.equal(inscriber.fullName, 'Sankar Ganesh');
 
-    assert.equal(inscriber.compute(), 1);
-    assert.equal(inscriber.value(), 1);
+    compute = function() {
+      return `Mr. ${this.firstName} ${this.lastName}`;
+    };
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
+    assert.equal(inscriber.fullName, 'Mr. Sankar Ganesh');
   });
 
-  it('check compute is not returnable', function() {
-    let compute = {};
-    let inscriber = Inscriber(compute);
+  it('check inscriber compute allows overriding properties', function() {
+    let inscriber = new Inscriber();
+    inscriber.firstName = 'Sankar';
+    inscriber.lastName = 'Ganesh';
+    let compute = function() {
+      return `${this.firstName} ${this.lastName}`;
+    };
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
+    assert.equal(inscriber.fullName, 'Sankar Ganesh');
 
-    assert.deepEqual(inscriber.compute, compute);
-    assert.equal(inscriber.value(), undefined);
+    compute = function() {
+      return `Mr. ${this.firstName} ${this.lastName} ${this.designation}`;
+    };
+    inscriber.designation = 'MSc';
+    inscriber.compute('fullName', ['firstName', 'lastName', 'designation'], compute);
+    assert.equal(inscriber.fullName, 'Mr. Sankar Ganesh MSc');
+  });
+
+  it('check inscriber compute allows multiple property bindings', function() {
+    let inscriber = new Inscriber();
+    inscriber.firstName = 'Sankar';
+    inscriber.lastName = 'Ganesh';
+    let compute = function() {
+      return `${this.firstName} ${this.lastName}`;
+    };
+    inscriber.compute('fullName', ['firstName', 'lastName'], compute);
+    assert.equal(inscriber.fullName, 'Sankar Ganesh');
+    inscriber.street = 'bull street, london';
+    let computeAddress = function() {
+      return `${this.firstName} ${this.lastName} ${this.street}`;
+    };
+    inscriber.compute('address', ['firstName', 'lastName', 'street'], computeAddress);
+    assert.equal(inscriber.fullName, 'Sankar Ganesh');
+    assert.equal(inscriber.address, 'Sankar Ganesh bull street, london');
   });
 });
